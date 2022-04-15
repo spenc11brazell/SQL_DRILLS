663904fa70a6888adeb79ce9f6d5be402f8c47bf
Spencer Brazell <sa.brazell11@gmail.com>
2022-04-14 22:17:19 -0600

    update table working

---
+++ update.sql
@@ -0,0 +1,21 @@
+-- 1
+UPDATE customer
+SET fax = null
+WHERE fax IS NOT null;
+-- 2
+UPDATE customer
+SET company = 'self'
+WHERE company IS null
+-- 3
+UPDATE customer 
+SET last_name = 'Thompson' 
+WHERE first_name = 'Julia' AND last_name = 'Barnett'
+-- 4
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl'
+-- 5
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = (SELECT genre_id FROM genre WHERE name = 'Metal')
+and composer is null
