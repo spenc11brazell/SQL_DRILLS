cddb8f9bbe37d8ce64eec13acb182b4644b5a6d8
Spencer Brazell <sa.brazell11@gmail.com>
2022-04-14 22:38:30 -0600

    delete table working

--- delete.sql
+++ delete.sql
@@ -8,3 +8,39 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+
+-- 1
+create table animals (
+id serial primary key,
+name varchar(450),
+type text,
+age int
+);
+-- 2
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+-- 3
+select 
+	*
+from animals
+-- 4
+delete
+from animals
+where type = 'lion'
+-- 5
+delete
+from animals
+where name like 'M%'
+-- 6
+delete
+from animals
+where age < 9
